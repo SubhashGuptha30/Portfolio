b0725e7f57711c0300fbc0465eb18614b432a4f5
Subhash Guptha <subbuguptha2005@outlook.com>
2025-11-29 23:22:01 +0530

    fix: add fallback for sanity env vars to fix build

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/sanity/schema'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '41nqleq3'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export default defineConfig({
     basePath: '/studio',
